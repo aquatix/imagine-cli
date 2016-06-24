4c3eaa6db5a20a0379b7e0b3dacf26caae28756a
Michiel Scholten <michiel@diginaut.net>
2016-06-24 14:43:24 +0200

    Moved all command line stuff into one file

--- imagine.py
+++ imagine.py
@@ -1,7 +1,10 @@
+import datetime
 import logging
 import os
 import sys
+import click
 from imagine_core import *
+from utilkit import fileutil
 from PIL import Image as PILImage, ImageFile as PILImageFile, ExifTags
 import exifread
 from hashlib import md5
@@ -24,6 +27,18 @@ else:
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 lh.setFormatter(formatter)
 logger.addHandler(lh)
+
+
+def prt(messageType, message):
+    if DEBUG == False:
+        return
+
+    if messageType == 'i':
+        print '[Info] {0}'.format(message)
+    elif messageType == 'e':
+        print '[Error] {0}'.format(message)
+    elif messageType == 'd':
+        print '[Debug] {0}'.format(message)
 
 
 def get_filename(directory, filename):
@@ -165,3 +180,48 @@ def walk_archive(collection, images_dir, archive_dir):
 
     return 42
 
+
+## Main program
+@click.group()
+def cli():
+    """
+    imagine image archive
+    """
+    pass
+
+
+@cli.command()
+@click.option('-i', '--inputdir', prompt='Input/source directory path', help='Input/source directory path')
+@click.option('-a', '--archivedir', prompt='Archive/target directory path', help='Archive/target directory path')
+def update_archive(inputdir, archivedir):
+    print('Updating or creating archive in {0} from source at {1}'.format(archivedir, inputdir))
+
+    # expand directories and appending / if needed
+    inputdir = os.path.join(inputdir,'')
+    archivedir = os.path.join(archivedir,'')
+
+    fileutil.ensure_dir_exists(archivedir)
+
+    print 'Scanning {0}'.format(inputdir)
+
+    db_file = os.path.join(archivedir, 'imagine_cli.db')
+    should_create = True
+    print db_file
+    if os.path.exists(db_file):
+        should_create = False
+
+    imagine.database = imagine.SqliteDatabase(db_file)
+    imagine.BaseModel.database = imagine.database
+
+    imagine.init_db(db_file)
+    if should_create:
+        imagine.create_archive()
+
+    imagine.update_collection('test', 'test-slug', inputdir, archivedir)
+
+
+if __name__ == '__main__':
+    """
+    imagine is ran standalone
+    """
+    cli()
